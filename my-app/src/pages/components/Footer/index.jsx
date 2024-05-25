73d4de933800fbac846937fd23a1ca2fff68e09e
Nikol Karnikova <ni.karnikova@gmail.com>
2024-05-25 17:05:46 +0200

    footer CSS

--- my-app/src/pages/components/Footer/index.jsx
+++ my-app/src/pages/components/Footer/index.jsx
@@ -1,24 +1,23 @@
-import mapaSrc from './mapa.png';
+import "./style.css";
+import mapaSrc from "./mapa.png";
 export const Footer = () => {
   return (
     <div className="footer--container">
-      <div className="Contact">
+      <div className="contact">
         <h2>Kontakt</h2>
-        <ul>
-          <li>Hotel Stříbrava</li>
-          <li>Ke Kamenné lávce 12 </li>
-          <li>217 24 Libnice nad Stříbravou</li>
+        <p>
+          Hotel Stříbrava <br />
+          Ke Kamenné lávce 12
           <br />
-          <li>
-            <a href="mailto:recepce@hotelstribrava.cz">
-              recepce@hotelstribrava.cz
-            </a>
-          </li>
-        </ul>
+          217 24 Libnice nad Stříbravou
+          <br />
+          <br />
+          <a href="mailto:recepce@hotelstribrava.cz">
+            recepce@hotelstribrava.cz
+          </a>
+        </p>
       </div>
-      <div>
-        <img className="footer--image" src={mapaSrc} alt="Mapa" />
-      </div>
+      <img className="footer--image" src={mapaSrc} alt="Mapa" />
     </div>
   );
 };
